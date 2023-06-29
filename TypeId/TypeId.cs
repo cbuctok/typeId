8d5506d14547a390b72562885dffb97d0132721d
Grigory Evseev <grigorye@in2.si>
2023-06-29 19:46:27 +0200

    Conceptual init

---
+++ TypeId/TypeId.cs
@@ -0,0 +1,266 @@
+﻿namespace TypeId
+{
+    using System;
+
+    /// <summary>
+    /// TypeId is UUID that contains a prefix and a UUID suffix. The prefix is used to id the type
+    /// of the object. The suffix is used to identify the object.
+    /// </summary>
+    public struct TypeId : IComparable, IComparable<TypeId>, IEquatable<TypeId>, IFormattable
+    {
+        private const char _delimeter = '_';
+        public readonly Guid Guid => ToGuid();
+        public string Id { get; private set; }
+        public string Type { get; private set; }
+
+        public static TypeId FromGuid(string prefix, Guid guid)
+        {
+            return new TypeId
+            {
+                Type = prefix,
+                Id = FromGuid(guid)
+            };
+        }
+
+        public static bool IsValidPrefix(string prefix)
+        {
+            if (prefix.Length == 0)
+            {
+                return false;
+            }
+
+            foreach (var c in prefix)
+            {
+                // false if not ascii or delimeter
+                if (c > 127 || c == _delimeter)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsValidSuffix(string suffix)
+        {
+            if (suffix.Length != 26)
+            {
+                return false;
+            }
+
+            foreach (var c in suffix)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static TypeId NewTypeId(string prefix)
+        {
+            return new TypeId
+            {
+                Type = prefix.ToLower(),
+                Id = NewB32UUid()
+            };
+        }
+
+        public static TypeId NewTypeId()
+        {
+            return new TypeId
+            {
+                Type = string.Empty,
+                Id = NewB32UUid()
+            };
+        }
+
+        public static bool operator !=(TypeId left, TypeId right)
+        {
+            return !(left==right);
+        }
+
+        public static bool operator <(TypeId left, TypeId right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(TypeId left, TypeId right)
+        {
+            return left.CompareTo(right)<=0;
+        }
+
+        public static bool operator ==(TypeId left, TypeId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator >(TypeId left, TypeId right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(TypeId left, TypeId right)
+        {
+            return left.CompareTo(right)>= 0;
+        }
+
+        public static TypeId Parse(string s)
+        {
+            var parts = s.Split(_delimeter);
+            if (parts.Length != 2)
+            {
+                throw new ArgumentException($"Invalid TypeId format - expected prefix{_delimeter}suffix");
+            }
+
+            // validate prefix
+            if (!IsValidPrefix(parts[0]))
+            {
+                throw new ArgumentException("Invalid TypeId format - incorrect prefix");
+            }
+
+            // validate suffix
+            if (!IsValidSuffix(parts[1]))
+            {
+                throw new ArgumentException("Invalid TypeId format - incorrect suffix");
+            }
+
+            var guid = SimpleBase.Base32.Rfc4648.Decode(parts[1]);
+            if (guid.Length != 16)
+            {
+                throw new ArgumentException("Invalid TypeId format - expected suffix in base32");
+            }
+
+            return new TypeId
+            {
+                Type = parts[0],
+                Id = parts[1]
+            };
+        }
+
+        public static bool TryParse(string s, out TypeId typeId)
+        {
+            try
+            {
+                typeId = Parse(s);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                typeId = default;
+                return false;
+            }
+        }
+
+        public readonly int CompareTo(object obj)
+        {
+            if (obj is TypeId other)
+                return CompareTo(other);
+
+            throw new ArgumentException("Object is not a TypeId");
+        }
+
+        public readonly int CompareTo(TypeId other)
+        {
+            var typeCompare = string.CompareOrdinal(Type, other.Type);
+            if (typeCompare != 0)
+            {
+                return typeCompare;
+            }
+
+            return string.CompareOrdinal(Id, other.Id);
+        }
+
+        public override readonly bool Equals(object? obj)
+        {
+            return obj is TypeId id
+                && Equals(id);
+        }
+
+        public readonly bool Equals(TypeId other)
+        {
+            return Id == other.Id
+                && Type == other.Type;
+        }
+
+        public override readonly int GetHashCode()
+        {
+            return HashCode.Combine(Id, Type);
+        }
+
+        public void SetId(Guid guid)
+        {
+            Id = FromGuid(guid);
+        }
+
+        public void SetId(string suffix)
+        {
+            if (!IsValidSuffix(suffix))
+            {
+                throw new ArgumentException("Invalid TypeId format - incorrect suffix");
+            }
+
+            Id = suffix;
+        }
+
+        public void SetType(string prefix)
+        {
+            if (!IsValidPrefix(prefix))
+            {
+                throw new ArgumentException("Invalid TypeId format - incorrect prefix");
+            }
+
+            Type = prefix;
+        }
+
+        // Returns the guid in "registry" format.
+        public override readonly string ToString() => ToString("d", null);
+
+        public readonly string ToString(string? format)
+        {
+            return ToString(format, null);
+        }
+
+        public readonly string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            var registryFormatted = Type + _delimeter + Id;
+
+            if (string.IsNullOrWhiteSpace(format))
+            {
+                return registryFormatted.ToLowerInvariant();
+            }
+
+            formatProvider ??= System.Globalization.CultureInfo.CurrentCulture;
+
+            return formatProvider.ToString().ToLowerInvariant() switch
+            {
+                "g" => registryFormatted,
+                _ => registryFormatted.ToLowerInvariant(),
+            };
+        }
+
+        private static string FromGuid(Guid guid)
+        {
+            return SimpleBase.Base32.Rfc4648.Encode(guid.ToByteArray()).ToLowerInvariant();
+        }
+
+        private static string NewB32UUid()
+        {
+            var guid = Guid.NewGuid();
+            return FromGuid(guid);
+        }
+
+        private readonly Guid ToGuid()
+        {
+            var guid = SimpleBase.Base32.Rfc4648.Decode(Id);
+            if (guid.Length != 16)
+            {
+                throw new ArgumentException("Invalid TypeId format - expected suffix in base32");
+            }
+
+            return new Guid(guid);
+        }
+    }
+}
---
+++ TypeIdTests/TypeIdGeneralTests.cs
@@ -0,0 +1,127 @@
+namespace TypeIdTests
+{
+    using System.Diagnostics;
+    using TypeId;
+
+    [TestClass]
+    public class TypeIdGeneralTests
+    {
+        [TestMethod]
+        public void Equal()
+        {
+            var typeId = TypeId.NewTypeId("prefix1");
+            var typeId2 = TypeId.Parse(typeId.ToString());
+            Assert.AreEqual(typeId, typeId2, "Parsing");
+
+            typeId2 = new TypeId();
+            typeId2.SetId(typeId.Id);
+            typeId2.SetType(typeId.Type);
+            Assert.AreEqual(typeId, typeId2, "Setters");
+        }
+
+        [TestMethod]
+        public void GuidTests()
+        {
+            var guid = Guid.NewGuid();
+            var typeId = TypeId.FromGuid("prefix", guid);
+            Assert.AreEqual(guid, typeId.Guid, "Guid roundtrip");
+
+            var typeId2 = new TypeId();
+            typeId2.SetId(guid);
+            typeId2.SetType(typeId.Type);
+            Assert.AreEqual(typeId, typeId2, "Setters");
+            Assert.AreEqual(guid, typeId2.Guid, "Guid roundtrip2");
+        }
+
+        [TestMethod]
+        public void NotEqual()
+        {
+            var typeId = TypeId.NewTypeId("prefix1");
+            var typeId2 = TypeId.NewTypeId("prefix1");
+            Assert.AreNotEqual(typeId, typeId2);
+        }
+
+        [TestMethod]
+        public void PerformanceTest()
+        {
+            // run a thousand times to make sure we don't get an equal sign and use Stopwatch to
+            // measure the time
+            const int iterations = int.MaxValue / 1_000;
+            var hashSetOfIds = new HashSet<TypeId>();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (var i = 0; i < iterations; i++)
+            {
+                hashSetOfIds.Add(TypeId.NewTypeId("prefix1"));
+            }
+            stopwatch.Stop();
+
+            const decimal timePerIterationMilliseconds = (decimal)0.003;
+
+            var elapsedMilliseconds = (decimal)stopwatch.ElapsedMilliseconds;
+
+            // assert it takes less than timePerIterationMilliseconds per iteration
+            Assert.IsTrue(elapsedMilliseconds < iterations * timePerIterationMilliseconds, $"Time per iteration: {elapsedMilliseconds / iterations} ms");
+            Console.WriteLine($"Time to run {iterations} iterations: {elapsedMilliseconds} ms, {elapsedMilliseconds / iterations} ms per iteration");
+
+            // assert we have the expected number of unique ids
+            Assert.AreEqual(iterations, hashSetOfIds.Count, "Unique ids");
+        }
+
+        [TestMethod]
+        public void SortingTest()
+        {
+            // create a list of type ids
+            var typeIds = new List<TypeId>();
+            for (var i = 0; i < 10; i++)
+            {
+                typeIds.Add(TypeId.NewTypeId("prefix" + i));
+            }
+
+            // typeIds are sortable so the list should be sorted without any extra work assert the
+            // list is sorted
+            Assert.IsTrue(typeIds.SequenceEqual(typeIds.OrderBy(x => x)), "Sorting");
+        }
+
+        [TestMethod]
+        public void TryParse()
+        {
+            var typeId = TypeId.NewTypeId("prefix1");
+            Assert.IsTrue(TypeId.TryParse(typeId.ToString(), out var typeId2));
+            Assert.AreEqual(typeId, typeId2, "Parsing");
+        }
+
+        [TestMethod]
+        public void TryParseFail()
+        {
+            // Missing prefix
+            Assert.IsFalse(TypeId.TryParse(string.Empty, out var typeId2));
+            Assert.AreEqual(default, typeId2, "Missing prefix");
+
+            // Missing suffix
+            Assert.IsFalse(TypeId.TryParse("prefix1", out typeId2));
+            Assert.AreEqual(default, typeId2, "Missing suffix");
+
+            // Invalid prefix
+            Assert.IsFalse(TypeId.TryParse("prefix1_", out typeId2));
+            Assert.AreEqual(default, typeId2, "Invalid prefix");
+
+            // Invalid suffix
+            Assert.IsFalse(TypeId.TryParse("prefix1_1234567890123456789012345", out typeId2));
+            Assert.AreEqual(default, typeId2, "Invalid suffix");
+
+            // Invalid prefix and suffix
+            Assert.IsFalse(TypeId.TryParse("prefix__xW2NU3TAO4KE7AR7CETWTJAUGI", out typeId2), "Invalid prefix and suffix");
+            Assert.AreEqual(default, typeId2, "Invalid prefix and suffix");
+        }
+
+        [TestMethod]
+        public void TryParseFromString()
+        {
+            const string typeIdExample = "prefix_DW2NU3TAO4KE7AR7CETWTJAUGI";
+            Assert.IsTrue(TypeId.TryParse(typeIdExample, out var typeId2), "Parsing example");
+            Assert.IsTrue(TypeId.IsValidPrefix(typeId2.Type), "Validate prefix");
+            Assert.IsTrue(TypeId.IsValidSuffix(typeId2.Id), "Validate suffix");
+        }
+    }
+}
